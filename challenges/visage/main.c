5d9af5959878415b8df1101767b702653b1babbf
salim <lesalgeriensdesstars@gmail.com>
2025-04-08 20:37:13 +0100

    le vzaaaageeee

--- challenges/visage/main.c
+++ challenges/visage/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv){
-    int j = 0; scanf("%d", &j);
-    srand(j); j = rand();
-    srand(0xface);
-    if(j==rand())puts("<flag>");
+int main(){
+    srand(0xface); int j = 0; scanf("%d", &j);
+    if(j==rand()) puts("<flag>");
     else puts("hint: face");
     return 0;
 }
