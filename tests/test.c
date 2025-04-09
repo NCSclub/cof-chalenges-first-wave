2ec3a68fa4d9485886a1ea8ae89726b332027900
salim <lesalgeriensdesstars@gmail.com>
2025-04-09 19:39:42 +0100

    deckerized webbbbb

--- tests/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    if(argc<2) return 0;
-    char* s; sprintf(s, "echo %s", argv[1]);
-    system(s);
-    return 0;
-}
