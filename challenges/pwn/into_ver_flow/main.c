169cb42969c033b953a218179d31ee2b155a661c
salim <lesalgeriensdesstars@gmail.com>
2025-04-09 01:15:54 +0100

    drahemmmmm

--- challenges/pwn/into_ver_flow/main.c
+++ challenges/pwn/into_ver_flow/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
     int xtal = 4294967200; int tha; 
-    scanf("%d", &tha); for(; tha!=0; tha--) xtal++;
+    for(scanf(" %d", &tha); tha!=0; tha--) xtal++;
     if(xtal==1) puts("<flag>"); return 0;
 }
